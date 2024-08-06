fe8e4167477510604c0952823283db3173b7f2e4
Isabelle Reny <68451719+LostReny@users.noreply.github.com>
2024-08-06 09:31:41 -0300

    ParticleSystem

---
+++ Assets/Scripts/Screen/ButtonStartGame.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonStartGame : MonoBehaviour
+{
+
+    public ParticleSystem particleSystem; 
+
+    public void OnClick()
+    {
+        particleSystem.Play();
+    }
+}
